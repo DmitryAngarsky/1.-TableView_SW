9f4ad6bfb8fcdc1ddf59cedc07cb402d6a55ffaa
Dmitry Tarakanov <femto8032@gmail.com>
2019-11-19 22:31:08 +0400

    App icon Launch screen Row hight

--- 1. TableView_SW/MainViewController.swift
+++ 1. TableView_SW/MainViewController.swift
@@ -28,9 +28,19 @@ class MainViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
         cell.textLabel?.text = restorauntNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restorauntNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 3
+        cell.imageView?.clipsToBounds = true
+        
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
  
     /*
