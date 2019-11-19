29ac07c653fd4101a38591001053ce49485b27fe
Dmitry Tarakanov <femto8032@gmail.com>
2019-11-19 21:05:22 +0400

    Initial Commit

---
+++ 1. TableView_SW/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainViewController.swift
+//  1. TableView_SW
+//
+//  Created by Дмитрий Тараканов on 16.11.2019.
+//  Copyright © 2019 Dmitry Angarsky. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let cellIdentifier  = "Cell"
+    let restorauntNames = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай",
+                           "Классик", "Шок", "Bonsai", "Burger Heroes", "Kitchen",
+                           "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy", "X.O"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restorauntNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = restorauntNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restorauntNames[indexPath.row])
+        return cell
+    }
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
